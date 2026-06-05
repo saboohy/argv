d93ffa74761cbe115bd702b0f2ad64b381273c20
Sabuhi Alizada <sabuhi.alizada@hotmail.com>
2026-06-05 06:10:12 +0400

    Complete parser tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -6,8 +6,55 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use Saboohy\Argv\Parser;
+use Saboohy\Argv\Lexer;
+use Saboohy\Argv\Token;
+use Saboohy\Argv\Exception\UnexpectedTokenException;
 
 final class ParserTest extends TestCase
 {
-    
+    #[Test]
+    #[TestDox('Throws an exception when an unexpected token is entered')]
+    public function throwsAnExceptionWhenAnUnexpectedTokenIsEntered(): void
+    {
+        $failTokens = [
+            0 => [
+                new Token(type: "T_DASH", value: "-"),
+                new Token(type: "T_LITERAL_0", value: "flag"),
+                new Token(type: "T_EQUAL", value: "=")
+            ]
+        ];
+
+        $parser = new Parser($failTokens);
+        
+        $this->expectException(UnexpectedTokenException::class);
+
+        $parser->parse();
+    }
+
+    #[Test]
+    #[TestDox('It parses valid tokens successfully')]
+    public function parsesValidTokensSuccessfully(): void
+    {
+        $lexer = new Lexer(["make:it", "file_name", "--option=value", "-flag", "--opt=val"]);
+        $lexer->tokenize();
+
+        $parser = new Parser($lexer->tokens());
+        $parser->parse();
+
+        $expected = [
+            "arguments" => [
+                "make:it",
+                "file_name"
+            ],
+            "options" => [
+                "option" => "value",
+                "opt" => "val"
+            ],
+            "flags" => [
+                "flag" => true
+            ]
+        ];
+
+        $this->assertSame($parser->elements(), $expected);
+    }
 }

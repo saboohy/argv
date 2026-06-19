a04a4ab18c19d9c453917fb02f4a73076a1c6f72
Sabuhi Alizada <sabuhi.alizada@hotmail.com>
2026-06-20 03:25:55 +0400

    Emit semantic tokens for CLI arguments

--- src/Lexer.php
+++ src/Lexer.php
@@ -29,33 +29,29 @@ class Lexer
      */
     public function tokenize(): void
     {
-        $pattern = "/^" . sprintf(
-            "%s?%s%s?%s?",
-            "(?P<T_DASH>-{1,2})",
-            "(?P<T_LITERAL_0>[a-zA-Z0-9\-_:\.\/\\\\]*)",
-            "(?P<T_EQUAL>={1})",
-            "(?P<T_LITERAL_1>[a-zA-Z0-9\-_:\.\/\\\\]*)"
-        ) . "$/";
-
         foreach ( $this->vectors as $index => $value ) {
 
-            $match = preg_match($pattern, $value, $matches);
+            if ( preg_match("/^-{1,2}([a-zA-Z\-_]+)={1}(.+)$/", $value, $option_matches) ) {
+                list($opt_key, $opt_val) = array_slice($option_matches, 1);
+                
+                $this->tokens[$index][] = new Token(type: "T_OPTION_KEY", value: $opt_key);
+                $this->tokens[$index][] = new Token(type: "T_OPTION_VAL", value: $opt_val);
+            }
+            elseif ( preg_match("/^-{1,2}([a-zA-Z\-_]+)$/", $value, $flag_matches) ) {
+                $flag_key = $flag_matches[1];
 
-            if ( !$match ) {
-                throw new UnexpectedCharacterException("Unexpected character detected in vector: $value");
-            };
+                $this->tokens[$index][] = new Token(type: "T_FLAG_KEY", value: $flag_key);
+            }
+            elseif ( preg_match("/^([a-zA-Z][a-zA-Z0-9\-_\.\:\/\\\\]+)$/", $value, $command_matches) ) {
+                $cmd_key = $command_matches[1];
 
-            unset($matches[0]);
-            
-            $filtered = array_filter($matches, function($value, $key) {
-                return !is_int($key) && $value !== "";
-            }, ARRAY_FILTER_USE_BOTH);
-
-            $this->tokens[$index] = array_map(
-                fn($key, $val) => new Token(type: $key, value: $val),
-                array_keys($filtered),
-                $filtered
-            );;
+                $this->tokens[$index][] = new Token(type: "T_ARGUMENT", value: $cmd_key);
+            }
+            else {
+                throw new UnexpectedCharacterException(
+                    sprintf("Unexpeted character at %s", $value)
+                );
+            }
         }
     }
 
